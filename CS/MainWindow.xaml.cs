5512c915971710fb30db33b926d8c186d3e67080
dxIvan <38530056+ivansupport@users.noreply.github.com>
2018-12-26 17:33:38 +0300

    add example content

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.Utils.Serializing;
+using DevExpress.Xpf.Core.Serialization;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace DXSample {
+    public partial class MainWindow : Window, INotifyPropertyChanged {
+        const string LayoutFilePath = "Layout.xml";
+        const string AppName = "TestApplication";
+
+        string _testProperty;
+        [XtraSerializableProperty]
+        public string TestProperty {
+            get { return _testProperty; }
+            set {
+                _testProperty = value;
+                RaisePropertyChanged(nameof(TestProperty));
+            }
+        }
+
+        public MainWindow() {
+            DataContext = this;
+            DXSerializer.SetSerializationID(this, "test");
+            DXSerializer.AddCustomGetSerializablePropertiesHandler(this, CustomGetSerializableProperties);
+            InitializeComponent();
+        }
+
+        private void CustomGetSerializableProperties(object sender, CustomGetSerializablePropertiesEventArgs e) {
+            e.SetPropertySerializable(WidthProperty, new DXSerializable());
+            e.SetPropertySerializable(HeightProperty, new DXSerializable());
+        }
+
+        void OnInitialized(object sender, System.EventArgs e) {
+            if (File.Exists(LayoutFilePath))
+                DXSerializer.Deserialize(this, LayoutFilePath, AppName, new DXOptionsLayout());
+        }
+        void OnClosing(object sender, CancelEventArgs e) {
+            DXSerializer.Serialize(this, LayoutFilePath, AppName, new DXOptionsLayout());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
